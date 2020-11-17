337cc39226459bbbb10c0d206d1b46741da5a368
cunguyendev <cunguyen.dev@gmail.com>
2020-11-17 16:57:47 +0700

    Add the current time rendering.

--- src/components/text-paragraph/index.tsx
+++ src/components/text-paragraph/index.tsx
@@ -1,6 +1,22 @@
-import React, { MouseEvent } from 'react';
+import React, { MouseEvent, useEffect, useState } from 'react';
 
 const TextParagraph = () => {
+  const [timenow, updateTimeNow] = useState(new Date());
+
+  const tick = () => {
+    updateTimeNow(new Date());
+  };
+
+  useEffect(() => {
+    const timeData = setInterval(() => {
+      tick();
+    }, 1000)
+
+    return () => {
+      clearInterval(timeData);
+    };
+  }, []);
+
   const ctaHandler = (event: MouseEvent) => {
     // tslint:disable-next-line: no-console
     console.log('You are clicked on the button!')
@@ -11,6 +27,7 @@ const TextParagraph = () => {
     <div className="text-paragragh">
       <h2 className="text-paragragh__title">Lorem, ipsum dolor.</h2>
       <p className="text-paragragh__description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint earum dolorum ipsam est non tempore, unde ducimus modi iusto molestias?</p>
+      <p className="time-now">Time now: {`${timenow.toLocaleString()}`}</p>
       <div className="text-paragragh__cta">
         <a onClick={ctaHandler} href="/" className="button">About us</a>
       </div>
@@ -18,4 +35,4 @@ const TextParagraph = () => {
   );
 }
 
-export default TextParagraph;
+export default React.memo(TextParagraph);
