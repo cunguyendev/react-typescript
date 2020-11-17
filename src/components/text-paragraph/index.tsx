da2f4e6456034849bb0a9ac92ed97d6a831733e3
cunguyendev <cunguyen.dev@gmail.com>
2020-11-17 14:26:33 +0700

    Update event handling.

--- src/components/text-paragraph/index.tsx
+++ src/components/text-paragraph/index.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { MouseEvent } from 'react';
 
 const TextParagraph = () => {
-  const ctaHandler = () => {
+  const ctaHandler = (event: MouseEvent) => {
     // tslint:disable-next-line: no-console
-    console.log('hello mnab')
+    console.log('You are clicked on the button!')
+    event.preventDefault();
   }
 
   return (
@@ -11,10 +12,7 @@ const TextParagraph = () => {
       <h2 className="text-paragragh__title">Lorem, ipsum dolor.</h2>
       <p className="text-paragragh__description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint earum dolorum ipsam est non tempore, unde ducimus modi iusto molestias?</p>
       <div className="text-paragragh__cta">
-        <a onClick={(e) => {
-          ctaHandler();
-          e.preventDefault();
-        }} href="/" className="button">About us</a>
+        <a onClick={ctaHandler} href="/" className="button">About us</a>
       </div>
     </div>
   );
