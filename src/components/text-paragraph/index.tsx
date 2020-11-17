0d7fd9cacc09744205956678c7726f8fdf136ead
cunguyendev <cunguyen.dev@gmail.com>
2020-11-17 13:49:18 +0700

    Add cta handler.

--- src/components/text-paragraph/index.tsx
+++ src/components/text-paragraph/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 
 const TextParagraph = () => {
+  const ctaHandler = () => {
+    // tslint:disable-next-line: no-console
+    console.log('hello mnab')
+  }
+
   return (
     <div className="text-paragragh">
       <h2 className="text-paragragh__title">Lorem, ipsum dolor.</h2>
       <p className="text-paragragh__description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint earum dolorum ipsam est non tempore, unde ducimus modi iusto molestias?</p>
       <div className="text-paragragh__cta">
-        <a href="/" className="button">About us</a>
+        <a onClick={(e) => {
+          ctaHandler();
+          e.preventDefault();
+        }} href="/" className="button">About us</a>
       </div>
     </div>
   );
