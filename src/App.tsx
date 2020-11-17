a308bc32248eedf6b323cf859c2db2ce7e26d461
cunguyendev <cunguyen.dev@gmail.com>
2020-11-17 11:20:58 +0700

    Update the header layout.

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,31 @@ import Header from './layouts/header';
 import Footer from './layouts/footer';
 import Wrapper from './layouts/wrapper';
 
+const navigationData = [
+  {
+    url: '/',
+    text: 'Home',
+    active: true
+  },
+  {
+    url: '/about/',
+    text: 'About'
+  },
+  {
+    url: '/projects/',
+    text: 'Projects'
+  }
+]
+
+const theApp = {
+  name:  'Code Lab',
+  baseUrl: '/'
+}
+
 const App = () => {
   return (
     <div className="App">
-      <Header />
+      <Header navigationData={navigationData} theApp={theApp}/>
       <Wrapper />
       <Footer />
     </div>
--- src/layouts/header/index.tsx
+++ src/layouts/header/index.tsx
@@ -1,20 +1,34 @@
-import React from 'react';
+import React, { FunctionComponent } from 'react';
 
-const Header = () => {
+interface Navigation {
+  url: string,
+  text: string,
+  active?:boolean
+}
+
+interface TheApp {
+  name: string,
+  baseUrl: string
+}
+
+interface HeaderProps {
+  navigationData: Navigation[];
+  theApp: TheApp
+}
+
+const Header: FunctionComponent<HeaderProps> = ({navigationData, theApp}: HeaderProps) => {
   return (
     <div className="header">
-      <h1 className="header__brand"><a href="/">Code Lab</a></h1>
+      <h1 className="header__brand"><a href={theApp.baseUrl}>{theApp.name}</a></h1>
       <div className="nav">
         <ul className="nav__items">
-          <li className="nav__items__item nav__items__item--active">
-            <a href="/">Home</a>
-          </li>
-          <li className="nav__items__item ">
-            <a href="/about/">About</a>
-          </li>
-          <li className="nav__items__item ">
-            <a href="/projects/">Projects</a>
-          </li>
+          {navigationData.map(navigation => {
+            return (
+              <li key={navigation.text} className={`nav__items__item ${navigation.active && 'nav__items__item--active'}`}>
+                <a href={navigation.url}>{navigation.text}</a>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>
